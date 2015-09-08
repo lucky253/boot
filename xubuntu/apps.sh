6bd026e8fa98f902dbc909bc998fe48640eeb41c
Lewis Hoffman <lewis.hoffman@gmail.com>
2015-09-08 17:38:47 -0600

    add xfce4 .config

--- xubuntu/apps.sh
+++ xubuntu/apps.sh
@@ -1,1 +1,20 @@
+# install basics
+sudo apt-get install ssh git emacs software-center gnome-terminal 
+
+
+# WEB: Download chrome
+sudo dpkg -i ~/Downloads/chrome-xxx.deb
+sudo apt-get install -f
+
+google-chrome-stable
+# WEB: log into chrome 
+
+# generate new ssh key
+ssh-keygen
+
+# WEB: install new ssh key in
+git clone git@github.com:leword/boot.git
+
+# Settings > Preferred Applications:  set gnome-terminal for terminal
+
 
