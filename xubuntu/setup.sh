467a9f32242c5c512ac1fa4749cd1c7a3106f39b
Lewis Hoffman <lewis.hoffman@gmail.com>
2015-09-08 20:52:36 -0600

    add rubymine config, & initial cut at keybindings

--- xubuntu/setup.sh
+++ xubuntu/setup.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # install basics
 sudo apt-get install ssh git emacs software-center gnome-terminal software-properties-common
 
@@ -12,22 +14,36 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global push.default simple
+
+
+######## MANUAL STEPS #########
 
 # WEB: Download chrome
 sudo dpkg -i ~/Downloads/chrome-xxx.deb
 sudo apt-get install -f
 
 google-chrome-stable
-# WEB: log into chrome 
+# WEB: log in:to chrome
 
 # generate new ssh key
 ssh-keygen
 
-# WEB: install new ssh key in
+# WEB: install new ssh key in github
+
+# WEB: Download RubyMine.
+
+######## END MANUAL STEPS #########
+
+
+
+
 git clone git@github.com:leword/boot.git
 
 # setup dotfile symlinks
-ln -s ~/.gconf ~/boot/dotfiles/.gconf
-ln -s ~/.bashrc ~/boot/dotfiles/.bashrc
-ls -s ~/.config/xfce4 ~/boot/dotfiles/.config/xfce4
-ls -s ~/.config/xfce4-session ~/boot/dotfiles/.config/xfce4-session
+cd ~
+ln -sf boot/dotfiles/.gconf .
+ln -sf ~/.bashrc ~/boot/dotfiles/.bashrc
+ls -sf ~/.config/xfce4 ~/boot/dotfiles/.config/xfce4
+ls -sf ~/.config/xfce4-session ~/boot/dotfiles/.config/xfce4-session
+ls -sf ~/.RubyMine70/config ~/boot/dotfiles/.config/xfce4-session
